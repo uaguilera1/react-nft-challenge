f8250bf8dfc7b080db9fe1361f518e42e1b136b1
ulises <ulises@tamu.edu>
2021-12-12 09:35:18 -0600

    Added Collection Cards
    Collection cards added

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
+import { useState, useEffect } from 'react';
 
 function App() {
   return(
     <div className="app">
       <Header />
+      <CollectionCard 
+        id={0} 
+        name={'Bandana Punk'} 
+        traits={[{'value': 7}]} 
+        image='https://nftlabs.mypinata.cloud/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu'
+      />
     </div>
   )
 }
